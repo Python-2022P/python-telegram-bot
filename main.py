dea6ce74c9af36cff1ca11bca9076ee1146b019c
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-28 19:40:00 +0500

    get text and id

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ bot = telegram.Bot(TOKEN)
 
 updates = bot.get_updates()
 
-print(updates[-1].message.text)
+last_update = updates[-1]
+
+print(last_update.message.chat.id)
+print(last_update.message.text)
