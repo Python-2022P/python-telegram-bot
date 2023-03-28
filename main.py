1ba3bd9263ac9aba891df1b59db09b6027065b71
abdurasulov-olqar <abdurasulovolqar123@gmail.com>
2023-03-28 19:37:02 +0500

    exo_bot

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import telegram 
 import os
+import time
+
+def main():
+    TOKEN = os.environ.get('TOKEN')
+
+    bot = telegram.Bot(TOKEN)
+    # for last update id
+    last_update_id = -1
+    while True:
+        # current update
+        curr_update = bot.get_updates()[-1]
+        # current update id
+        curr_update_id = curr_update.update_id
+        # check new update
+        if last_update_id != curr_update_id:
+            last_message = curr_update.message
+            # get data for send message
+            chat_id = last_message.chat.id
+            text = last_message.text
+            if text:
+                # send message
+                bot.send_message(chat_id, text)
+
+            last_update_id = curr_update_id
+        
+        time.sleep(1)
 
 
-TOKEN = os.environ.get('TOKEN')
-
-bot = telegram.Bot(TOKEN)
-
-print(bot.send_message('1258594598', 'ok'))
+main()
